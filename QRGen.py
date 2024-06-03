d9cbc23dd6befc6ffa301609445a06ce8fcb584a
Javier <106848262+javiiicz@users.noreply.github.com>
2024-06-02 21:13:49 -0600

    ennumerate steps

---
+++ QRGen.py
@@ -0,0 +1,13 @@
+# Step 1: Data Analysis
+
+# Step 2: Data Encoding
+
+# Step 3: Error Correction
+
+# Step 4: Structure Final Message
+
+# Step 5: Module Placement in Matrix
+
+# Step 6: Data Masking
+
+# Step 7: Format and Version Information
